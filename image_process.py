25cab71fea3253fb63dc3dec419eb6cf9c10cf42
Scott Forman <sjforman@gmail.com>
2013-10-08 11:12:00 -0400

    Initial commit. Basic page using Mapbox API.

---
+++ image_process.py
@@ -0,0 +1,7 @@
+""" process image to extract color
+""" given a file name, return a HEX value
+
+""" make a list of: 
+""" timestamp, filename, HEX value
+
+""" color the background of the page to that HEX value
