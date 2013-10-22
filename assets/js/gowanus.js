177071d46f8202d04328b066f70d11327fe269e9
Scott Forman <sjforman@gmail.com>
2013-10-22 12:41:33 -0400

    Some styling (webfont, active states)

--- assets/js/gowanus.js
+++ assets/js/gowanus.js
@@ -1,33 +1,21 @@
 var style = {
     fillColor: 'white',
     fillOpacity: 1.0,
-    weight:0
-}
+    weight: 0
+};
 
 function setColor(id) {
     style.fillColor = photos[id]["hexvalue"];
     document.body.style.background = style.fillColor;
     colorLayer.setStyle(style);
-    }
-
-function showImage(id) {
-    var selector = "img#" + id.replace(/\./, "\\\\.");
-    $(selector).toggleClass('hidden');
-    console.log(selector);
-    }
-
-function modPage(id) {
-    setColor(id);
-    showImage(id);
-}
+};
 
 var photos;
 var links = [];
 var datelinks = {};
 var sortable_datelinks = [];
-var photolinks = [];
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
     function getArray(){
         return $.getJSON("data/photodata/colordata.json");
@@ -36,27 +24,28 @@ $( document ).ready(function() {
     getArray().done(function(json) {
         photos = json;
 
-        var onclick = 'onclick="modPage(this.id);return false;">'
+        var onclick = 'onclick="setColor(this.id);return false;">'
 
         $.each( json, function( key, val ) {
             val = val["datetime"].replace(/:/, "/").replace(/:/, "/");
             datelinks[key] = val;
-            sortable_datelinks.push([key, datelinks[key]])
+            sortable_datelinks.push([key, datelinks[key]]);
         });
 
         dates_sorted = sortable_datelinks.sort(function(a,b) {return Date.parse(a[1]) - Date.parse(b[1])});
 
         for (var i = 0; i < sortable_datelinks.length; i++) {
-            links.push( "<li><a id='" + sortable_datelinks[i][0] + "' href='#'" + onclick + Date.parse(sortable_datelinks[i][1]).toString('M/d/yyyy hh:mm tt') + "</a></li>" );
-            photolinks.push( "<img class='thumb hidden' id=" + sortable_datelinks[i][0] + " src='./data/photodata/images/" + sortable_datelinks[i][0] + "'</img>")
-}
+            links.push( "<li><a id='" + sortable_datelinks[i][0] + "' href='#' class='datelink'" + onclick + Date.parse(sortable_datelinks[i][1]).toString('M/d/yyyy h:mm tt') + "</a></li>" );
+        }
 
         for (var i = 0; i < links.length; i++) {
             $(".photolist").append(links[i]);
-            $("#photos").append(photolinks[i]);
         }
 
-        //$('#photos').hide();
+        $('a.datelink').click(function(){
+            $('a.datelink').removeClass("active");
+            $(this).addClass("active");
+        });
 
     });
 
