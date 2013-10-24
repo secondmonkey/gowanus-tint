a3beeed182741af5eb364096d5c4df3b50c0753a
Scott Forman <sjforman@gmail.com>
2013-10-24 16:59:33 -0400

    Added navigation

--- assets/js/gowanus.js
+++ assets/js/gowanus.js
@@ -14,6 +14,7 @@ var photos;
 var links = [];
 var datelinks = {};
 var sortable_datelinks = [];
+var imglinks = [];
 
 $(document).ready(function() {
 
@@ -34,12 +35,24 @@ $(document).ready(function() {
 
         dates_sorted = sortable_datelinks.sort(function(a,b) {return Date.parse(a[1]) - Date.parse(b[1])});
 
+        // generate and place links for map page
+
         for (var i = 0; i < sortable_datelinks.length; i++) {
             links.push( "<li><a id='" + sortable_datelinks[i][0] + "' href='#' class='datelink'" + onclick + Date.parse(sortable_datelinks[i][1]).toString('M/d/yyyy h:mm tt') + "</a></li>" );
         }
 
         for (var i = 0; i < links.length; i++) {
             $(".photolist").append(links[i]);
+        }
+
+        // generate and place links for image page
+
+        for (var i = 0; i < sortable_datelinks.length; i++) {
+            imglinks.push( "<li><img src='./data/photodata/images/" + sortable_datelinks[i][0] + "' class='imglink' alt='" + sortable_datelinks[i][1] + "'</ img></li>" );
+        }
+
+        for (var i = 0; i < imglinks.length; i++) {
+            $("#imglinks").append(imglinks[i]);
         }
 
         $('a.datelink').click(function(){
